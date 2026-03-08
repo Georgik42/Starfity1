6ce6ebc44e552d4f54da9f5d2253f0d13f27e397
Georgik42 <giorgik12345678@gmail.com>
2026-03-08 13:21:42 +0100

    Create initial App component for mobile web

---
+++ apps/mobile-web/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{padding:20}}>
+      <h1>Starfity MVP</h1>
+      <p>App is running.</p>
+    </div>
+  );
+}
